dc8953498e3c67c0a076a0a00e1300250a621401
Brian Oblad <a.gray.cloud@gmail.com>
2019-10-02 15:02:00 -0400

    loading screen works

--- App.js
+++ App.js
@@ -6,6 +6,9 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
+import { FirebaseWrapper } from './firebase/firebase';
+import { firebaseConfig } from './firebase/config';
+
 import {
   LoginScreen,
   Feed,
@@ -50,3 +53,7 @@ export default class App extends React.Component {
     return <AppContainer />;
   }
 }
+// export default function App(props) {
+//   FirebaseWrapper.GetInstance().Initialize(firebaseConfig);
+//   return <AppContainer />
+// }
