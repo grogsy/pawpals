7dcf4806e8ccaffaa5c0c3d029a5d0e3d55d1cd0
Brian <a.gray.cloud@gmail.com>
2019-10-02 22:43:11 -0300

    feat(components) #1 lets user select on registration

--- components/Register/register.js
+++ components/Register/register.js
@@ -1,66 +1,68 @@
-import React from 'react';
-import { Text, View, TextInput, Button } from 'react-native';
+import React from "react";
+import { Text, View, TextInput, Button, Picker } from "react-native";
 
-import { db } from '../../firebase/config';
+import { db } from "../../firebase/config";
 
-import styles from './style';
+import styles from "./style";
 
 const DEFAULT_PIC =
-  'https://icon-library.net/images/profile-image-icon/profile-image-icon-25.jpg';
+  "https://icon-library.net/images/profile-image-icon/profile-image-icon-25.jpg";
 
 const {
   container,
   inputField,
   formField,
   inputFieldAndDescription,
-  description,
+  description
 } = styles;
 
 export default class RegistrationForm extends React.Component {
   static navigationOptions = {
-    title: 'Sign Up',
+    title: "Sign Up"
   };
 
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
-      password: '',
+      username: "",
+      password: "",
+      usertype: "adopter",
       usernameTaken: false,
-      emptyFields: false,
+      emptyFields: false
     };
     this.handleFormInput = this.handleFormInput.bind(this);
   }
 
   handleFormInput() {
-    const { username, password } = this.state;
+    const { username, password, usertype } = this.state;
     if (!username || !password) {
       this.setState({ emptyFields: true });
     } else {
-      db.ref('/users')
+      db.ref("/users")
         .child(username)
-        .once('value', snapshot => {
+        .once("value", snapshot => {
           if (snapshot.exists()) {
             this.setState({ usernameTaken: true });
           } else {
             let newUser = {
               username,
               password,
-              firstname: '',
-              lastname: '',
+              usertype,
+              firstname: "",
+              lastname: "",
               imgurl: DEFAULT_PIC,
-              phone: '',
-              email: '',
-              location: '',
-              bio: '',
+              phone: "",
+              email: "",
+              location: "",
+              bio: ""
             };
 
             db.ref(`/users/${username}`).set({
-              ...newUser,
+              ...newUser
             });
 
-            this.props.navigation.navigate('Profile', {
-              user: newUser,
+            this.props.navigation.navigate("Profile", {
+              user: newUser
             });
           }
         });
@@ -89,9 +91,22 @@ export default class RegistrationForm extends React.Component {
             />
           </View>
         </View>
+        <View>
+          <Text style={description}>
+            I Want To Register As Someone That Is:
+          </Text>
+          <Picker
+            selectedValue={this.state.usertype}
+            style={{ height: 50, width: 210 }}
+            onValueChange={(usertype, index) => this.setState({ usertype })}
+          >
+            <Picker.Item label="Looking To Adopt A Dog" value="adopter" />
+            <Picker.Item label="Putting Up An Adoption" value="provider" />
+          </Picker>
+        </View>
         <Text>
-          {this.state.usernameTaken ? 'That handle is taken' : ''}
-          {this.state.emptyFields ? 'Fill out both name and password' : ''}
+          {this.state.usernameTaken ? "That handle is taken" : ""}
+          {this.state.emptyFields ? "Fill out both name and password" : ""}
         </Text>
         <Button title="Register" onPress={this.handleFormInput} />
       </View>
